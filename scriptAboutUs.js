7840a380fcf68cb13a09395c3475b4e2e9076088
hadayasyahla <hadayasyahla@gmail.com>
2022-10-22 19:11:22 +0700

    create-aboutus

---
+++ scriptAboutUs.js
@@ -0,0 +1,20 @@
+if (localStorage.getItem("indexLogin") == 0) {
+    document.getElementById("login").style.display = "none";
+  } else {
+    document.getElementById("dash").style.display = "none";
+    document.getElementById("edit").style.display = "none";
+    document.getElementById("log").style.display = "none";
+  }
+  if (localStorage.getItem("roleUser")) {
+    if (localStorage.getItem("roleUser") == 1) {
+      document.getElementById("addEvent").style.display = "none";
+    }
+  } else {
+    document.getElementById("addEvent").style.display = "none";
+  }
+  
+  function signOut() {
+    localStorage.setItem("indexLogin", 1);
+    window.location.href = "loginPage.html";
+    localStorage.removeItem("roleUser");
+  }
